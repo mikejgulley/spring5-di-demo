c028f2e6ef24545f756d937e7f1909a303478efc
Michael Gulley <mgulley.mograph@gmail.com>
2017-08-29 18:19:37 -0400

    Shows how to use @ComponentScan to tell what base package to scan
    Find all components starting with ones you list

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -1,7 +1,6 @@
 package guru.springframework.controllers;
 
-import guru.springframework.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
+import guru.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 /**
